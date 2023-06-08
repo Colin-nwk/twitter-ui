45951ac65af6787bf4745143f4a661588366a195
colin-nwk <collinsnwoko05@gmail.com>
2023-06-08 18:49:55 +0100

    typed added and setup

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -17,3 +17,23 @@ export default {
     tabIconSelected: tintColorDark,
   },
 };
+
+// const tintColorLight = '#219ebc';
+// const tintColorDark = '#8ecae6';
+
+// export default {
+//   light: {
+//     text: '#0a1128',
+//     background: '#f8f7ff',
+//     tint: tintColorLight,
+//     tabIconDefault: '#ccc',
+//     tabIconSelected: tintColorLight,
+//   },
+//   dark: {
+//     text: '#f8f7ff',
+//     background: '#023047',
+//     tint: tintColorDark,
+//     tabIconDefault: '#ccc',
+//     tabIconSelected: tintColorDark,
+//   },
+// };
